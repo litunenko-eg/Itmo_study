97df2a8d18b3dcb0b284f54864bb7a594e52af8f
litunenko-eg <98417934+litunenko-eg@users.noreply.github.com>
2023-01-22 17:26:29 +0300

    Create ipython_log.py

---
+++ ipython_log.py
@@ -0,0 +1,40 @@
+# IPython log file
+
+bin(123456)
+#[Out]# '0b11110001001000000'
+bin(0b00110011 | 0b10000000)
+#[Out]# '0b10110011'
+a = 0;
+-10 <= a <= 100
+#[Out]# True
+a = 150;
+-10 <= a <= 100
+#[Out]# False
+x = 42
+y = x << 34
+x << 34
+#[Out]# 721554505728
+a = 13.8
+int(a)
+#[Out]# 13
+a = 13.888
+a.__ceil__()
+#[Out]# 14
+a.__floor__()
+#[Out]# 13
+round(a, 2)
+#[Out]# 13.89
+int(a)
+#[Out]# 13
+if (a-int(a))*10 >=5:
+    print (int(a+1))
+else:
+    print (int(a))
+    
+a = 13.444
+if (a-int(a))*10 >=5:
+    print (int(a+1))
+else:
+    print (int(a))
+    
+exit()
