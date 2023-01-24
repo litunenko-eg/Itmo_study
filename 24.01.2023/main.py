565a69e7fea74a8924d61fefee3f2ba089660a3c
litunenko-eg <98417934+litunenko-eg@users.noreply.github.com>
2023-01-24 23:15:02 +0300

    Create main.py

---
+++ 24.01.2023/main.py
@@ -0,0 +1,25 @@
+#1 task
+print('Задание 1. Строка \'Python is the best programming language in the world\' с 6го символа по -7й включительно:')
+str = 'Python is the best programming language in the world'
+str1 = str[6:-6]
+print(str1)
+print()
+
+print('Задание 2. Каждый третий символ строки \'Guido van Rossum is the benevolent dictator for life\':')
+str = 'Guido van Rossum is the benevolent dictator for life'
+str1 = str[::3]
+print(str1)
+print()
+
+print('Задание 3. Из строки \'You have a problem with authority, Mr. Anderson.\' получить словарь, где ключ -это символ, \nа значение - это количество повторений символа в строке:')
+str = 'You have a problem with authority, Mr. Anderson.'
+s = set(str)
+listt = list(str)
+#print(s)
+m = list(map(listt.count, s))
+#print(list(map(listt.count, s)))
+d = dict(zip(s, m))
+d1 = dict(zip(set(str), list(map(listt.count, s))))
+print(d)
+print (d1)
+print(d == d1)
